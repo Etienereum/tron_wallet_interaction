ba6b50a00e77768f2d297f60faa0240c5d3964e7
Etienereum <okeke_etienne@yahoo.com>
2021-06-11 19:06:24 +0100

    finished

--- src/wallet.js
+++ src/wallet.js
@@ -9,14 +9,24 @@
  * 8) Transfer Back all funds to 1st account.
 */
 const fs = require('fs');
-const tronweb = require('tronweb');
 const fetch = require('node-fetch');
 const hdWallet = require('tron-wallet-hd');
 const utils = hdWallet.utils;
 
+const baseURL = 'https://api.shasta.trongrid.io'
+
+const TronWeb = require('tronweb')
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider(baseURL);
+const solidityNode = new HttpProvider(baseURL);
+const eventServer = new HttpProvider(baseURL);
+const privateKey = "840be81abd92c3451de57fd440c1f00bcd23714e110a71ec40b80f431f5a5260";
+const tronWeb = new TronWeb(fullNode, solidityNode, eventServer);
+
 let accountArr = {}
 let privkeyArr = []
-let addressMaster, hexAddrMaster
+let privkeyMaster
+let addressMaster
     
 const generateSeed = async () => {
     const seedphrase = utils.generateMnemonic();
@@ -44,15 +54,13 @@ const createWallet = async () => {
     if (isValidSeed) {
         accountArr = await utils.generateAccountsWithMnemonic(seedphrase, 10);
     } else
-        throw new Error("Bad Seed Phrase")
-    // console.log(accountArr)
+        throw new Error("Read Bad Seed Phrase")
 
     // Write Private keys to a txt file
     for (let acc in accountArr) {
-        for (let privateKey in accountArr[acc]) {
-            if (privateKey == 'privateKey') {
-                // console.log(accountArr[acc][privateKey])
-                privkeyArr.push(accountArr[acc][privateKey])
+        for (let privKey in accountArr[acc]) {
+            if (privKey == 'privateKey') {
+                privkeyArr.push(accountArr[acc][privKey])
             }
         }
     }
@@ -61,39 +69,47 @@ const createWallet = async () => {
         if (err) {
             return console.log(err);
         }
-        console.log("The file was saved!");
+        console.log("All Private Keys have been saved!");
     });
 
+    privkeyMaster = privkeyArr[0]
+
     // Read 1st Private Key from the txt and funded it Manually
-    let MasterPK = privkeyArr[0]
-    const isValidPK = utils.validatePrivateKey(MasterPK)
+    const isValidPK = utils.validatePrivateKey(privkeyMaster)
 
     if (isValidPK) {
-        addressMaster = await utils.getAccountFromPrivateKey(MasterPK);
-        console.log(addressMaster)
-
-        hexAddrMaster = tronweb.address.toHex(addressMaster)
-        console.log(hexAddrMaster)
+        addressMaster = await utils.getAccountFromPrivateKey(privkeyMaster);
+        console.log({ addressMaster })
     }
-    let bal = await getBalance(hexAddrMaster)
-    console.log('Main Addr Bal', bal)
+    let balMaster = await tronWeb.trx.getBalance(addressMaster)
+    console.log(`'Master Address Balance is, ${balMaster}`)
 
     // Read other Private keys and fund them from the 1st account
     for (let i = 1; i < privkeyArr.length; i++){
         let address = await utils.getAccountFromPrivateKey(privkeyArr[i])
-        let hexAddr = tronweb.address.toHex(address)
-        let bal = await getBalance(hexAddr)
-        console.log(privkeyArr[i], address, hexAddr, bal);
-        console.log(hexAddrMaster, hexAddr, 10)
+        let tx = await tronWeb.trx.sendTransaction(address, 10000000, privkeyMaster);
 
-        let transfer = await blockchainTX(hexAddrMaster, hexAddr, 10)
-        // console.log(transfer)
-        // if (transfer) {
-            // Display all Wallet addresses.
-            // let balance = await getBalance(hexAddr)
-            // console.log(`Balance of ${address}, is ${balance}`)
-        // } else
-        //     throw new Error('Tranfer was not successful')
+        if (tx.result) {
+            //Display all Wallet addresses.
+            let balance = await tronWeb.trx.getBalance(address)
+            console.log(`${i}) Balance of ${address}, is ${balance}`)
+        } else
+            throw new Error(`Tranfer to ${address} was not successful`)
+    }
+
+    console.log("*****  Reversing all Transasactions  *****")
+
+    // Read other Private keys and fund them from the 1st account
+    for (let i = 1; i < privkeyArr.length; i++){
+        let address = await utils.getAccountFromPrivateKey(privkeyArr[i])
+        let tx = await tronWeb.trx.sendTransaction(addressMaster, 10000000, privkeyArr[i] );
+
+        if (tx.result) {
+            //Display all Wallet addresses.
+            let balance = await tronWeb.trx.getBalance(address)
+            console.log(`${i}) Balance of ${address}, is ${balance}`)
+        } else
+            throw new Error(`Tranfer from ${address} was not successful`)
     }
 }
 
@@ -107,28 +123,6 @@ const getBalance = async (hexAddr) => {
 
     return fetch(url, options).then(res => res.json())
         .then((json) => { return json.balance })
-        .catch(err => console.error('error:' + err));
-}
-
-const blockchainTX = async (sender, receiver, amount) => {
-    const url = 'https://api.shasta.trongrid.io/wallet/createtransaction';    
-    const options = {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json', 'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "to_address": receiver,
-            "owner_address!": sender,
-            "amount": amount
-        })
-    };
-
-    return fetch(url, options).then(res => res.json())
-        .then((json) => {
-            console.log(json)
-            return json
-        })
         .catch(err => console.error('error:' + err));
 }
 
